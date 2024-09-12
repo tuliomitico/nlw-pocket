487c199f2049a069c285447d9d01420ecec9cefa
Túlio de Freitas Castro <tuliofc2008@hotmail.com>
2024-09-12 16:59:43 -0300

    feat: add db postgres

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "drizzle-kit";
+import { env } from "./src/env";
 
 export default defineConfig({
-	schema: "./schema.ts",
+	schema: "./src/db/schema.ts",
 	out: "./.migrations",
 	dialect: "postgresql",
 	dbCredentials: {
-		url: process.env.DATABASE_URL,
+		url: env.DATABASE_URL,
 	},
 });
